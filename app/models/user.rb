6c0acf5d709ea7644a6f1faaa58b071b05701598
mikebeaujean <mbeaujean88@gmail.com>
2014-05-13 14:43:24 -0400

    quick user model edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
-  validates :name, presence: true
-  validates :email, presence: true, uniqueness: true, case_sensitive: false
-  validates(:password, { :length       => { :minimum => 8, :maximum => 16 },
-                         :presence     => true,
-                         :confirmation => true })
+  # validates :name, presence: true
+  # validates :email, presence: true, uniqueness: true, case_sensitive: false
+  # validates(:password, { :length       => { :minimum => 8, :maximum => 16 },
+  #                        :presence     => true,
+  #                        :confirmation => true })
   has_secure_password
 
 end
