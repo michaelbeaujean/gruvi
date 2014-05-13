7b4827be3db47bc515395be612333ffe8b04c2bd
mikebeaujean <mbeaujean88@gmail.com>
2014-05-12 21:12:05 -0400

    commenting out metronome and precompiling assets

--- app/assets/javascripts/metronome.js
+++ app/assets/javascripts/metronome.js
@@ -1,21 +1,20 @@
 
 
-$(document).ready(function(){
+// $(document).ready(function(){
 
-  // $("#bpm-submit").on("click", function(){
-  //   bpm = $("#bpm").val();
-  // })
+//   // $("#bpm-submit").on("click", function(){
+//   //   bpm = $("#bpm").val();
+//   // })
 
-  function metronomeTick() {
-   $("#m1").animate( {borderWidth: "5px"}, 1000*60/120, function()
-    $("#m1").animate( {borderWidth: "1px"} );
-    // T("perc", {r:1000}, A).on("ended", function() {
-    //   this.pause();
-    // }).bang().play();
-    metronomeTick();
-   });
-  }
+//   function metronomeTick() {
+//    $("#m1").animate( {borderWidth: "5px"}, 1000*60/120, function(){
+//     $("#m1").animate( {borderWidth: "1px"} );
+//     // T("perc", {r:1000}, A).on("ended", function() {
+//     //   this.pause();
+//     // }).bang().play();
+//     metronomeTick();
+//   }
 
-  metronomeTick();
+//   metronomeTick();
 
-});
+// });
