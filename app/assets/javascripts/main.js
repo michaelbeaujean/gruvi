9b76aae6babddf35ce2e1092b7ea98d2062cc394
generationloss <hwilliamstites@gmail.com>
2014-05-15 18:00:47 -0400

    added pusher code to main.js and welcome_controller.rb

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -206,12 +206,29 @@ function endGame(){
 	}, (eighthNote * 4000));
 };
 
-function startMetronome(){ setInterval(function(){
-	playSound(ABuffer, 0);}, (eighthNote * 2000));
+Pusher.log = function(message) {
+  if (window.console && window.console.log) {
+    window.console.log(message);
+  }
 };
 
-var metroGnome = startMetronome;
+var pusher = new Pusher('72df76eb0f3ff7e19b92');
+var channel = pusher.subscribe('test_channel');
+channel.bind('my_event', function(data) {
+  alert(data.message);
+});
 
-function stopMetronome(){
-	clearInterval(metroGnome);	
-}
+// function startMetronome(){ setInterval(function(){
+// 	playSound(ABuffer, 0);}, (eighthNote * 2000));
+// };
+
+// function startMetronome(){ 
+// 	metroGnome = setInterval(function(){
+// 		playSound(ABuffer, 0);}, (eightNote * 2000));
+// };
+
+// var metroGnome = startMetronome;
+
+// function stopMetronome(){
+// 	clearInterval(metroGnome);
+// }
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,4 +4,11 @@ class WelcomeController < ApplicationController
     @user = User.new
   end
 
+	def pusher
+		Pusher.url = "http://72df76eb0f3ff7e19b92:1d63892b5ce19dc9e5d0@api.pusherapp.com/apps/74865"
+
+		Pusher['test_channel'].trigger('my_event', {
+		  message: 'hello world'
+		})
+	end
 end
