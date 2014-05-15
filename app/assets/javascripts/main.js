5f8437653e5e23a43715e5a2f7cefa5177b5ab42
generationloss <hwilliamstites@gmail.com>
2014-05-14 21:12:56 -0400

    wrote a goddamn working simon game

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,7 +1,13 @@
 var simonNotes = [];
 var simonCounter = 0;
+var playerNotes = [];
+var playersTurn = false;
+var correctResponse = true;
+var simonDuration = simonCounter * eighthNote;
 
-var  DICTIONARY = {
+// The keys are the length of the individual note buffers; the values are the hex codes
+// for the colors their circles flash.  The Dictionary is ordered from low to high notes.
+var DICTIONARY = {
 	64849: "#FFFF9C",
 	47556: "#9CD2FF",
 	43744: "#A6FF9C",
@@ -13,9 +19,9 @@ var  DICTIONARY = {
 };
 
 $(document).ready(function(){
-	// var p = $("<p>").text("play");
+	var play = $("<p>").text("play");
 
-	// $("#gameplay-box").append(p);
+	$("#gameplay-box").append(play);
 	
 	$("#64849").on('click', function(){
 		playSound(ABuffer, 0);
@@ -41,22 +47,126 @@ $(document).ready(function(){
 	$("#43601").on('click', function(){
 		playSound(HighABuffer, 0);
 	});
+
+	$("#gameplay-box").on('click', function(){
+		startGame();
+	});
 });
 
-function playSimon() {
-	simonCounter++;
+// makes the circles flash
+function blink(div, color) {
+    $(div).stop().css("background-color", color).animate({ backgroundColor: "#FFFFFF"}, 1500);
+};
 
-	var startTime = 0;
+// plays a tone and makes the associated circle flash
+function playSound(buffer, startTime) {
+	// accesses the buffer's length to use as a key for the dictionary, then gets the color
+	// value
+	var length = buffer.length;
+	var color = DICTIONARY[length];
+	var div = "#" + length;
 
-	var note = _.sample(bufferLoader.bufferList);
-	simonNotes.push(note);
+	// WebAudioAPI boilerplate I copied from the docs
+	var source = context.createBufferSource();
+	source.buffer = buffer;
+	source.connect(context.destination);
 
-	for (i=0; i<simonNotes.length; i++) {
-		playSound(simonNotes[i], startTime);
-		startTime += eighthNoteTime;
+	// actually plays the note and blinks the div
+	setTimeout(function(){
+		source.noteOn(0);
+		blink(div, color)
+	}, (startTime * 1000));
+
+	// if it's the player's turn, adds the note to his/her array of responses
+	if (playersTurn) {
+		playerNotes.push(buffer);
+		getPlayerInput(buffer);
 	};
 };
 
-function blink(div, color) {
-    $(div).stop().css("background-color", color).animate({ backgroundColor: "#FFFFFF"}, 1500);
-}
+// adds a note to the computer-generated sequence, plays the sequence, then prompts the
+// player to repeat it
+function playSimon() {
+
+	simonCounter++;
+
+	// creates a slight pause before the round starts
+	var startTime = eighthNote;
+
+	// selects a random note of the scale and pushes it to the computer-generated sequence
+	var note = _.sample(bufferLoader.bufferList);
+	simonNotes.push(note);
+
+	// plays the notes in the sequence, an eighth-note apart
+	for (i=0; i<simonNotes.length; i++) {
+		playSound(simonNotes[i], startTime);
+		startTime += eighthNote;
+	};
+
+	var nowYou = $("<p>").text("Now you!");
+
+	// an eighth-note after the computer sequence is finished playing, prompts the player
+	// to try to match the sequence
+	setTimeout(function(){
+		$("#gameplay-box").empty();
+		$("#gameplay-box").append(nowYou);
+		playersTurn = true;
+		getPlayerInput();
+	}, (simonDuration * 1000) + (eighthNote * 1000));
+};
+
+function startGame(){
+	$("#gameplay-box").empty();
+	playSimon();
+};
+
+// evaluates each note the player inputs to see if it matches the corresponding note in
+// the computer's sequence, whether the player has inputted the same number of notes
+// as are in the sequence, and, if so, if s/he matched the sequence correctly.  Either
+// begins a new round or ends the game, as appropriate.
+function getPlayerInput(note){
+	var responseLength = playerNotes.length;
+
+	// as long as the game hasn't just started (meaning there's no player input yet),
+	// compares the player's most recent note against the corresponding note in the
+	// computer-generated sequence.  If they don't match, sets correctResponse to false.
+	if ((responseLength > 0) && !(note === simonNotes[responseLength - 1])) {
+		correctResponse = false;
+	};
+
+	// if the length of the sequence the player has inputted matches the length of the
+	// computer's sequence, evaluates whether to begin another round or end the game.
+	if (responseLength === simonNotes.length) {
+		$("#gameplay-box").empty();
+		playerNotes = [];
+		playersTurn = false;
+	
+		if (correctResponse) {
+			var goodJob = $("<p>").text("Good job!");
+
+			// creates a double-length pause before beginning the next round
+			setTimeout(function(){
+				$("#gameplay-box").append(goodJob);
+				playersTurn = false;
+				playSimon();
+			}, (eighthNote * 2000));	
+		}
+		else {
+			var sorry = $("<p>").text("Sorry!");
+			var playAgain = $("<p>").text("click to play again");
+
+			simonNotes = [];
+			simonCounter = 0;
+			correctResponse = true;
+
+			$("#gameplay-box").append(sorry);
+
+			// creates a quadruple-length pause before offering the user a
+			// chance to play again
+			setTimeout(function(){
+				$("#gameplay-box").empty();
+				$("#gameplay-box").append(playAgain);
+			}, (eighthNote * 4000));
+		};
+	};
+};
